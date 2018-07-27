091a635e04a1c47d1d51f158ef3260fafb926f1a
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-07-28 01:35:54 +0600

    Describe in general category

--- test/index.test.js
+++ test/index.test.js
@@ -7,10 +7,12 @@ var chai = require('chai')
 chai.should()
 var expect = chai.expect
 
-it('should tell mocha and chai is available', () => {
-  assert(true)
-  'hello'.should.be.a('string')
-  expect('hello').not.be.equal('word')
+describe('General', () => {
+  it('should tell mocha and chai is available', () => {
+    assert(true)
+    'hello'.should.be.a('string')
+    expect('hello').not.be.equal('word')
+  })
 })
 
 describe('Tools', () => {
