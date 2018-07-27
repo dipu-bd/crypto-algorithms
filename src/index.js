f91b1f52999ef15d72949be5c11594a41c8248b6
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-07-27 14:14:57 +0600

    Add CaesarCipher alias

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-// Classics
+// Classic cryptography
 exports.ShiftCipher = require('./classic/shift-cipher');
+exports.CaesarCipher = exports.ShiftCipher;
+
 exports.AffineCipher = require('./classic/affine-cipher');
